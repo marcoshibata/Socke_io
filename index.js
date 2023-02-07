3267b29acaaa561d0e300af487fecb1e186f999e
Marco Shibata <66227743+marcoshibata@users.noreply.github.com>
2023-02-07 15:16:46 +0900

    chat concluido

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const http = require('http').createServer(app);
 
 const io = require('socket.io')(http);
 
-var ususarios = [];
+
+
+
+
+var usuarios = [];
+
+var socketIds = [];
+
+
+
+
 
 app.get('/', (req, res) => {
 
@@ -15,10 +25,72 @@ app.get('/', (req, res) => {
 
 
 io.on('connection',(socket)=>{
-    
-    socket.on('chat message',(obj)=>{
-        io.emit('chat messege',obj);
+
+
+
+    socket.on('new user',function(data){
+
+        if(usuarios.indexOf(data) != -1){
+
+            socket.emit('new user',{success: false});
+
+        }else{
+
+            usuarios.push(data);
+
+            socketIds.push(socket.id);
+
+            
+
+            socket.emit('new user',{success: true});
+
+            //Emit para os outros usuários dizendo que tem um novo usuário ativo.
+
+            //socket.broadcast.emit("hello", "world");
+
+        }
+
     })
+
+    
+
+    socket.on('chat message',(obj)=>{
+
+        if(usuarios.indexOf(obj.nome) != -1 && usuarios.indexOf(obj.nome) == socketIds.indexOf(socket.id)
+
+        ){
+
+            io.emit('chat message',obj);
+
+        }else{
+
+            console.log('Erro: Você não tem permissão para executar a ação.');
+
+        }
+
+        
+
+    })
+
+
+
+    socket.on('disconnect', () => {
+
+        let id = socketIds.indexOf(socket.id);
+
+        socketIds.splice(id,1);
+
+        usuarios.splice(id,1);
+
+        console.log(socketIds);
+
+        console.log(usuarios);
+
+        console.log('user disconnected');
+
+    });
+
+
 
 })
 
